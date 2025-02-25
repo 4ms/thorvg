70fe91cfda66d3307110a16795e9bcc70522a8e9
Dan Green <danngreen1@gmail.com>
2025-02-24 21:32:16 -0800

    Fix warning re: wrong type in printf

--- src/renderer/tvgInitializer.cpp
+++ src/renderer/tvgInitializer.cpp
@@ -67,7 +67,7 @@ static bool _buildVersionInfo(uint32_t* major, uint32_t* minor, uint32_t* micro)
     uint32_t microVal = atoi(p);
 
     char sum[7];
-    snprintf(sum, sizeof(sum), "%d%02d%02d", majorVal, minorVal, microVal);
+    snprintf(sum, sizeof(sum), "%d%02d%02d", (int)majorVal, (int)minorVal, (int)microVal);
     _version = atoi(sum);
 
     if (major) *major = majorVal;
