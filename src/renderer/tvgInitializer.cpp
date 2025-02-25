5ede3d617a5f67b770440df671d114a6c92ffe8f
Dan Green <danngreen1@gmail.com>
2025-02-24 21:32:35 -0800

    Remove operator new, operator delete

--- src/renderer/tvgInitializer.cpp
+++ src/renderer/tvgInitializer.cpp
@@ -173,11 +173,11 @@ uint16_t THORVG_VERSION_NUMBER()
 }
 
 
-void* operator new(std::size_t size) {
-    return tvg::malloc(size);
-}
+// void* operator new(std::size_t size) {
+//     return tvg::malloc(size);
+// }
 
 
-void operator delete(void* ptr) noexcept {
-    tvg::free(ptr);
-}
+// void operator delete(void* ptr) noexcept {
+//     tvg::free(ptr);
+// }
